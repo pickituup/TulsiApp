8de5c618073944e93b46671739601afc0aa68cf7
Artem Tuchonuk <irascible.dog@gmail.com>
2017-08-19 14:20:05 +0300

    Fixing

--- Tulsi/Tulsi/Pages/BuyerProfilePage.xaml.cs
+++ Tulsi/Tulsi/Pages/BuyerProfilePage.xaml.cs
@@ -9,14 +9,19 @@ using Tulsi.ViewModels;
 
 namespace Tulsi {
     public partial class BuyerProfilePage : ContentPage, IView {
+        private BuyerProfileViewModel _viewModel;
+
         public BuyerProfilePage() {
             InitializeComponent();
 
-            ProfileViewModel pvm = new ProfileViewModel();
-            ProfileTransactionsListView.ItemsSource = pvm.TransactionsData;
-            ProfileTransactionsListView.ItemSelected += (sender, e) => {
-                ((ListView)sender).SelectedItem = null;
-            };
+            _viewModel = new BuyerProfileViewModel();
+            BindingContext = _viewModel;
+
+            //ProfileViewModel pvm = new ProfileViewModel();
+            //ProfileTransactionsListView.ItemsSource = pvm.TransactionsData;
+            //ProfileTransactionsListView.ItemSelected += (sender, e) => {
+            //    ((ListView)sender).SelectedItem = null;
+            //};
 
             TapGestureRecognizer CloseTap = new TapGestureRecognizer();
             CloseTap.Tapped += (s, e) => {
---
+++ Tulsi/Tulsi/ViewModels/BuyerProfileViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tulsi.Model;
+using Tulsi.MVVM.Core;
+
+namespace Tulsi.ViewModels {
+    public sealed class BuyerProfileViewModel : ViewModelBase {
+        /// <summary>
+        /// Public ctor.
+        /// </summary>
+        public BuyerProfileViewModel() {
+            TransactionsData = new List<ProfileTransaction>()
+            {
+                new ProfileTransaction { Code = "SKC", Number = "28", IsP=true, Quantity="8,200" },
+                new ProfileTransaction { Code = "SKC", Number = "28", IsP=false, Quantity="8,200" },
+                new ProfileTransaction{ Code = "SKC", Number = "28", IsP=true, Quantity="8,200" },
+                new ProfileTransaction { Code = "SKC", Number = "28", IsP=true, Quantity="8,200" },
+            };
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<ProfileTransaction> TransactionsData { get; set; }
+    }
+}
