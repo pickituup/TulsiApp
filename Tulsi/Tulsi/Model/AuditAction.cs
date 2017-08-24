aaddd04e501ff9f77fec258b342321c8bb1bc92a
Artem Tuchonuk <irascible.dog@gmail.com>
2017-08-24 19:07:17 +0300

    Audit log page

--- Tulsi/Tulsi/Model/AuditAction.cs
+++ Tulsi/Tulsi/Model/AuditAction.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Tulsi.Model {
-    public  class AuditEntry {
+    public sealed class AuditAction {
         /// <summary>
         /// 
         /// </summary>
-        public DateTime Date { get; set; }
+        public string Action { get; set; }
 
         /// <summary>
         /// 
         /// </summary>
-        public List<AuditAction> AuditActions { get; set; }
+        public string Code { get; set; }
     }
 }
--- Tulsi/Tulsi/ViewModels/AuditLogViewModel.cs
+++ Tulsi/Tulsi/ViewModels/AuditLogViewModel.cs
@@ -14,43 +14,114 @@ using Xamarin.Forms;
 namespace Tulsi.ViewModels {
     public class AuditLogViewModel : ViewModelBase, IViewModel {
 
-        ObservableCollection<AuditEntry> _auditData;
+        private ObservableCollection<AuditEntry> _auditData;
+
+        /// <summary>
+        ///     Public ctor().
+        /// </summary>
+        public AuditLogViewModel() {
+            DisplaySearchPageCommand = new Command(() => {
+                BaseSingleton<ViewSwitchingLogic>.Instance.NavigateTo(ViewType.SearchPage);
+            });
+
+            HARDCODED_DATA_INSERT();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ICommand DisplaySearchPageCommand { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public ObservableCollection<AuditEntry> AuditData {
             get { return _auditData; }
             set { SetProperty(ref _auditData, value); }
         }
 
-        AuditEntry _selectedMenuItem;
-        public AuditEntry SelectedMenuItem {
-            get { return _selectedMenuItem; }
-            set {
-                if (SetProperty(ref _selectedMenuItem, value) && value != null) {
-                    // Do something
-                }
-            }
-        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dispose() {
 
-        public ICommand DisplaySearchPageCommand { get; private set; }
+        }
 
         /// <summary>
-        ///     ctor().
+        /// 
         /// </summary>
-        public AuditLogViewModel() {
-            AuditData = new ObservableCollection<AuditEntry>()
-            {
-                new AuditEntry { Action = "Modified Buyer", Code = "SKC" },
-                new AuditEntry { Action = "Changed Rates", Code = "SKC" },
-                new AuditEntry { Action = "Deposit Received", Code = "MKC" },
-                new AuditEntry { Action = "Modified Buyer", Code = "SKC" },
-                new AuditEntry { Action = "Changed Rates", Code = "SKC" },
-                new AuditEntry { Action = "Deposit Received", Code = "MKC" },
+        private void HARDCODED_DATA_INSERT() {
+            AuditData = new ObservableCollection<AuditEntry>() {
+                new AuditEntry() { Date = DateTime.Now,
+                    AuditActions = new List<AuditAction>() {
+                        new AuditAction() { Action = "Modified Buyer", Code = "SKC" },
+                        new AuditAction() { Action = "Changed Rates", Code = "SKC"  },
+                        new AuditAction() { Action = "Deposit Received", Code = "MKC" },
+                        new AuditAction() { Action = "Modified Buyer", Code = "SKC"  }
+                    }
+                },
+                new AuditEntry() { Date = DateTime.Now,
+                    AuditActions = new List<AuditAction>() {
+                        new AuditAction() { Action = "Modified Buyer", Code = "SKC" },
+                        new AuditAction() { Action = "Changed Rates", Code = "SKC"  },
+                        new AuditAction() { Action = "Deposit Received", Code = "MKC" },
+                        new AuditAction() { Action = "Modified Buyer", Code = "SKC"  },
+                        new AuditAction() { Action = "Changed Rates", Code = "SKC"  },
+                        new AuditAction() { Action = "Deposit Received", Code = "MKC" }
+                    }
+                },
+                new AuditEntry() { Date = DateTime.Now,
+                    AuditActions = new List<AuditAction>() {
+                        new AuditAction() { Action = "Modified Buyer", Code = "SKC" },
+                        new AuditAction() { Action = "Changed Rates", Code = "SKC"  },
+                        new AuditAction() { Action = "Deposit Received", Code = "MKC" }
+                    }
+                },
+                new AuditEntry() { Date = DateTime.Now,
+                    AuditActions = new List<AuditAction>() {
+                        new AuditAction() { Action = "Modified Buyer", Code = "SKC" },
+                        new AuditAction() { Action = "Changed Rates", Code = "SKC"  },
+                        new AuditAction() { Action = "Deposit Received", Code = "MKC" },
+                        new AuditAction() { Action = "Modified Buyer", Code = "SKC"  },
+                        new AuditAction() { Action = "Changed Rates", Code = "SKC"  },
+                        new AuditAction() { Action = "Deposit Received", Code = "MKC" }
+                    }
+                },
+                new AuditEntry() { Date = DateTime.Now,
+                    AuditActions = new List<AuditAction>() {
+                        new AuditAction() { Action = "Modified Buyer", Code = "SKC"  },
+                        new AuditAction() { Action = "Changed Rates", Code = "SKC"  },
+                        new AuditAction() { Action = "Deposit Received", Code = "MKC" }
+                    }
+                },
+                new AuditEntry() { Date = DateTime.Now,
+                    AuditActions = new List<AuditAction>() {
+                        new AuditAction() { Action = "Modified Buyer", Code = "SKC" },
+                    }
+                },
+                new AuditEntry() { Date = DateTime.Now,
+                    AuditActions = new List<AuditAction>() {
+                        new AuditAction() { Action = "Modified Buyer", Code = "SKC" },
+                        new AuditAction() { Action = "Changed Rates", Code = "SKC"  },
+                        new AuditAction() { Action = "Deposit Received", Code = "MKC" },
+                        new AuditAction() { Action = "Modified Buyer", Code = "SKC"  },
+                        new AuditAction() { Action = "Changed Rates", Code = "SKC"  },
+                        new AuditAction() { Action = "Deposit Received", Code = "MKC" },
+                        new AuditAction() { Action = "Changed Rates", Code = "SKC"  },
+                        new AuditAction() { Action = "Deposit Received", Code = "MKC" },
+                        new AuditAction() { Action = "Modified Buyer", Code = "SKC"  },
+                        new AuditAction() { Action = "Changed Rates", Code = "SKC"  },
+                        new AuditAction() { Action = "Deposit Received", Code = "MKC" }
+                    }
+                },
+                new AuditEntry() { Date = DateTime.Now,
+                    AuditActions = new List<AuditAction>() {
+                        new AuditAction() { Action = "Modified Buyer", Code = "SKC" },
+                        new AuditAction() { Action = "Changed Rates", Code = "SKC"  },
+                        new AuditAction() { Action = "Deposit Received", Code = "MKC" }
+                    }
+                }
             };
-
-            DisplaySearchPageCommand = new Command(() => BaseSingleton<ViewSwitchingLogic>.Instance.NavigateTo(ViewType.SearchPage));
-        }
-
-        public void Dispose() {
-            
         }
     }
 }
